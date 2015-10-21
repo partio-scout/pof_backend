fb61627e25584f438c754415258146950453ccef
Oskari Kokko <oskari.kokko@iki.fi>
2015-10-21 18:43:58 +0300

    Added json page to view items in trash, so if someone had saved the tree, they can remove these items from that tree

---
+++ wp-content/themes/POFAPIWP/page-json-trash.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: JSON Trash
+*/
+
+header('Content-type: application/json');
+
+$ret = array();
+
+$args = array(
+	'numberposts' => -1,
+	'posts_per_page' => -1,
+	'post_type' => array('pof_post_program', 'pof_post_agegroup', 'pof_post_taskgroup', 'pof_post_task', 'pof_post_suggestion' ),
+	'post_status' => 'trash'
+);
+
+$the_query = new WP_Query( $args );
+
+if( $the_query->have_posts() ) {
+	while ( $the_query->have_posts() ) {
+		$tmp = new stdClass();
+		$the_query->the_post();
+		$post_id = $the_query->post->ID;
+		$tmp->guid = get_post_meta( $post_id, 'post_guid', true);
+		$tmp->title = $the_query->post->post_title;
+		$tmp->type = $the_query->post->post_type;
+		$tmp->modified = $the_query->post->post_modified;
+		array_push($ret, $tmp);
+	}
+}
+
+
+echo json_encode($ret);
+
+?>
