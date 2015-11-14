0d4b17170846f60df8dcf1488fa93f600d6a6e84
Oskari Kokko <oskari.kokko@iki.fi>
2015-11-14 21:19:38 +0200

    filter tags by namespace and lang

--- wp-content/themes/POFAPIWP/page-json-tags.php
+++ wp-content/themes/POFAPIWP/page-json-tags.php
@@ -5,14 +5,29 @@ Template Name: JSON Tags
 
 header('Content-type: application/json');
 
+$filter_langs = "all";
+$filter_tags = "all";
+
+
+if (isset($_GET["langs"]) && trim($_GET["langs"]) != ''){
+   $filter_langs = $_GET["langs"];
+}
+if (isset($_GET["tags"]) && trim($_GET["tags"]) != ''){
+   $filter_tags = $_GET["tags"];
+}
+
 $ret = new stdClass();
 
 $languages = pof_taxonomy_translate_get_languages();
 
 function pof_pages_get_tags($languages, $items, $item_tax_key) 
 {
+	global $filter_langs;
 	$items_arr = array();
 	foreach ($languages as $lang_key => $lang) {
+		if ($filter_langs != "all" && !strstr($filter_langs, $lang_key)) {
+			continue;
+		}
 		$tmp = new stdClass();
 		$tmp->lang = $lang_key;
 		$tmp->items = array();
@@ -34,47 +49,61 @@ function pof_pages_get_tags($languages, $items, $item_tax_key)
 	return $items_arr;
 }
 
+if ($filter_tags == "all" || strstr($filter_tags, "paikka")) {
+	// Places
+	$item_tax_key = 'place_of_performance';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->paikka = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
-// Places
-$item_tax_key = 'place_of_performance';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->paikka = pof_pages_get_tags($languages, $items, $item_tax_key);
+if ($filter_tags == "all" || strstr($filter_tags, "ryhmakoko")) {
+	// Groupsizes
+	$item_tax_key = 'groupsize';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->ryhmakoko = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
-// Groupsizes
-$item_tax_key = 'groupsize';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->ryhmakoko = pof_pages_get_tags($languages, $items, $item_tax_key);
+if ($filter_tags == "all" || strstr($filter_tags, "pakollisuus")) {
+	// Mandatory
+	$item_tax_key = 'mandatory';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->pakollisuus = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
-// Mandatory
-$item_tax_key = 'mandatory';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->pakollisuus = pof_pages_get_tags($languages, $items, $item_tax_key);
+if ($filter_tags == "all" || strstr($filter_tags, "suoritus_kesto")) {
+	//TaskDuration
+	$item_tax_key = 'taskduration';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->suoritus_kesto = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
-//TaskDuration
-$item_tax_key = 'taskduration';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->suoritus_kesto = pof_pages_get_tags($languages, $items, $item_tax_key);
+if ($filter_tags == "all" || strstr($filter_tags, "suoritus_valmistelu_kesto")) {
+	//TaskPreparationDuration
+	$item_tax_key = 'taskpreparationduration';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->suoritus_valmistelu_kesto = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
-//TaskPreparationDuration
-$item_tax_key = 'taskpreparationduration';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->suoritus_valmistelu_kesto = pof_pages_get_tags($languages, $items, $item_tax_key);
+if ($filter_tags == "all" || strstr($filter_tags, "tarvikkeet")) {
+	//Equipments
+	$item_tax_key = 'equpments';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->tarvikkeet = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
-//Equipments
-$item_tax_key = 'equpments';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->tarvikkeet = pof_pages_get_tags($languages, $items, $item_tax_key);
+if ($filter_tags == "all" || strstr($filter_tags, "taitoalueet")) {
+	//Taitoalueet
+	$item_tax_key = 'skillarea';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->taitoalueet = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
-//Taitoalueet
-$item_tax_key = 'skillarea';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->taitoalueet = pof_pages_get_tags($languages, $items, $item_tax_key);
-
-//Yleiset
-$item_tax_key = 'common';
-$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
-$ret->yleiset = pof_pages_get_tags($languages, $items, $item_tax_key);
-
+if ($filter_tags == "all" || strstr($filter_tags, "yleiset")) {
+	//Yleiset
+	$item_tax_key = 'common';
+	$items = pof_taxonomy_translate_get_items_by_taxonomy_base_key($item_tax_key);
+	$ret->yleiset = pof_pages_get_tags($languages, $items, $item_tax_key);
+}
 
 echo json_encode($ret);
 
