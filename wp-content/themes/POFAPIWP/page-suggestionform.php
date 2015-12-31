202b83c806ab96d2642ab110d0addf8e89377bc4
Oskari Kokko <oskari.kokko@iki.fi>
2015-12-31 13:18:01 +0200

    Suggestion for returning something in json

--- wp-content/themes/POFAPIWP/page-suggestionform.php
+++ wp-content/themes/POFAPIWP/page-suggestionform.php
@@ -60,22 +60,55 @@ if (   $_SERVER['REQUEST_METHOD'] === 'POST'
 	update_post_meta($suggestion_id, "pof_suggestion_writer", $_POST['suggetion_name']);
     update_post_meta($suggestion_id, "pof_suggestion_writer_id", $partio_id);
 
-    //TODO: send email
+    $suggestion_guid = get_post_meta( $suggestion_id, "post_guid", true );
 
     $emails_str = pof_settings_get_suggestions_emails();
 
-    echo "ff" . wp_mail( $emails_str, "[POF] Uusi vinkki", "Uusi vinkki, http://pof-backend.partio.fi/wp-admin/post.php?post=".$suggestion_id."&action=edit");
+    wp_mail( $emails_str, "[POF] Uusi vinkki", "Uusi vinkki, ".get_site_url()."/wp-admin/post.php?post=".$suggestion_id."&action=edit");
 
+	$return_val = 'json';
+	if (array_key_exists('return_val', $_POST)
+    && $_POST['return_val'] != "") {
+		$return_val = $_POST['return_val'];
+	}
 
-    echo pof_taxonomy_translate_get_translation_content("common", "suggestion_form_done", 0, $lang_key);
+    $location = "Location: " . $url=strtok($_SERVER["REQUEST_URI"],'?') . "?form_submit=ok&lang=" . $lang_key . "&return_val=" . $return_val;
 
-} else {
+    if ($suggestion_guid != "") {
+        $location .= "&suggestion_guid=" . $suggestion_guid;
+    }
 
+	header($location);
+	exit();
+//    echo pof_taxonomy_translate_get_translation_content("common", "suggestion_form_done", 0, $lang_key);
+
+}
+else {
     if (array_key_exists('lang', $_GET) && $_GET['lang'] != "") {
         $lang_key = $_GET['lang'];
     }
 
-get_header(); ?>
+    if (   array_key_exists('form_submit', $_GET) && $_GET['form_submit'] != ""
+        && array_key_exists('return_val', $_GET) && $_GET['return_val'] != "html") {
+        $tmp = new stdClass();
+        if ($_GET['form_submit'] == "ok") {
+            $tmp->status = "ok";
+            $tmp->message = pof_taxonomy_translate_get_translation_content("common", "suggestion_form_done", 0, $lang_key);
+            if (   array_key_exists('suggestion_guid', $_GET) && $_GET['suggestion_guid'] != "") {
+                $tmp->suggestion_guid = $_GET['suggestion_guid'];
+            }
+        } else {
+            $tmp->status = "error";
+            $tmp->message = pof_taxonomy_translate_get_translation_content("common", "suggestion_form_error", 0, $lang_key);
+        }
+        echo json_encode($tmp);
+        exit();
+    } 
+    else {
+        
+        get_header(); 
+		
+		?>
 
 
 	<div id="primary" class="content-area">
@@ -86,15 +119,25 @@ get_header(); ?>
 	            </header><!-- .entry-header -->
 
 			    <div class="entry-content">
+                    <?php
+                    if (array_key_exists('form_submit', $_GET) && $_GET['form_submit'] != "") {
+                        if ($_GET['form_submit'] == "ok") {
+                            echo '<h2>' . pof_taxonomy_translate_get_translation_content("common", "suggestion_form_done", 0, $lang_key) . "</h2>";
+                        } else {
+                            echo '<h2>' . pof_taxonomy_translate_get_translation_content("common", "suggestion_form_error", 0, $lang_key) . "</h2>";
+                        }
+                    }
+
+                        
+                    ?>
 		            <form action="" method="POST" class="tips__form">
+						<input type="hidden" name="return_val" value="html" />
 				        <input class="radius" type="text" name="suggetion_name" placeholder="<?php echo pof_taxonomy_translate_get_translation_content("common", "suggestion_form_name_placeholder", 0, $lang_key, true); ?> *" aria-label="Name" /><br /><br />
 				        <input class="radius" type="text" name="suggetion_title" placeholder="<?php echo pof_taxonomy_translate_get_translation_content("common", "suggestion_form_title_placeholder", 0, $lang_key, true); ?> *" aria-label="Title" /><br /><br />
 				        <textarea class="radius form-textarea" name="suggetion_content" placeholder="<?php echo pof_taxonomy_translate_get_translation_content("common", "suggestion_form_content_placeholder", 0, $lang_key, true); ?>"></textarea><br /><br />
 				        <input class="button radius" type="submit" name="submit-tip" value="<?php echo pof_taxonomy_translate_get_translation_content("common", "suggestion_form_sendbutton", 0, $lang_key, true); ?>" aria-label="Send" />
 
 	    		    </form>
-
-
                 </div>
             </article>
 		</main><!-- .site-main -->
@@ -102,4 +145,6 @@ get_header(); ?>
 
 <?php get_footer(); ?>
 
-<?php } ?>
+<?php    }
+    } 
+?>
