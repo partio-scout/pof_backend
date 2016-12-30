d4c7eebb60998ca10f9f637e9511457c3a5070c7
Oskari Kokko <oskari.kokko@iki.fi>
2016-12-30 22:45:06 +0200

    Fixes to suggestion form

--- wp-content/themes/POFAPIWP/page-suggestionform.php
+++ wp-content/themes/POFAPIWP/page-suggestionform.php
@@ -26,39 +26,38 @@ if (   $_SERVER['REQUEST_METHOD'] === 'POST'
     && $_POST['suggestion_content'] != "" ) {
 
     if (array_key_exists('lang', $_POST) && $_POST['lang'] != "") {
-        $lang_key = $_POST['lang'];
+        $lang_key = strtolower($_POST['lang']);
     }
     if (array_key_exists('partio_id', $_POST) && $_POST['partio_id'] != "") {
         $partio_id = $_POST['partio_id'];
     }
     if (array_key_exists('post_guid', $_POST) && $_POST['post_guid'] != "") {
-        $post_guid = $_POST['post_guid'];
+        $post_guid = trim($_POST['post_guid']);
     }
     $wp_error = false;
 
     $suggestion_title = "";
 
     if (   array_key_exists('suggestion_title', $_POST)
-        && $_POST['suggestion_title'] != "") {
+        && trim($_POST['suggestion_title']) != "") {
         $suggestion_title = mb_convert_encoding(trim($_POST['suggestion_title']),"UTF-8", "auto");
     }
 
     if ($suggestion_title == "") {
-        switch ($lang) {
+        switch ($lang_key) {
             default:
             case "fi":
-                $suggestion_title = "Toteutusvinkki";
+                $suggestion_title = "Toteutusvinkki ";
                 break;
             case "sv":
-                $suggestion_title = "Tips";
+                $suggestion_title = "Tips ";
                 break;
             case "en":
-                $suggestion_title = "Example";
+                $suggestion_title = "Example ";
                 break;
         }
 
         if ($post_guid != "") {
-
 
             $args = array(
 		        'numberposts' => -1,
@@ -79,12 +78,12 @@ if (   $_SERVER['REQUEST_METHOD'] === 'POST'
 
                 if ($parent_post_id > 0) {
 
-                    $parent_post_title = get_post_meta( $parent_post_id->ID, "title_".$lang, true );
+                    $parent_post_title = get_post_meta( $parent_post_id->ID, "title_".$lang_key, true );
 
                     if ($parent_post_title != "") {
-                        $suggestion_title .= " " + $parent_post_title;
+                        $suggestion_title .= ": " + $parent_post_title;
                     } else {
-                        $suggestion_title .= " " + $parent_post->post_title;
+                        $suggestion_title .= ": " + $parent_post->post_title;
                     }
 
                 }
@@ -195,7 +194,7 @@ if (   $_SERVER['REQUEST_METHOD'] === 'POST'
         }
     }
     update_post_meta($suggestion_id, "pof_suggestion_lang", $lang_key);
-	update_post_meta($suggestion_id, "pof_suggestion_writer", mb_convert_encoding(trim($_POST['suggestion_name']),"UTF-8", "auto"));
+    update_post_meta($suggestion_id, "pof_suggestion_writer", mb_convert_encoding(trim($_POST['suggestion_name']),"UTF-8", "auto"));
     update_post_meta($suggestion_id, "pof_suggestion_writer_id", $partio_id);
 
     $suggestion_guid = get_post_meta( $suggestion_id, "post_guid", true );
@@ -221,9 +220,16 @@ if (   $_SERVER['REQUEST_METHOD'] === 'POST'
 
 
     } else {
-        $content .= $mypost->post_title."\n\n";
+        $lang_title = '';
+        if ($lang_key != 'fi') {
+            $parent_post_title = get_post_meta( $mypost->ID, "title_".$lang_key, true );
+            if (trim($parent_post_title) != "") {
+                $lang_title = " (".$parent_post_title.")";
+            }
+        }
+        $content .= $mypost->post_title.$lang_title."\n\n";
     }
-    $content .= "Vinkin otsikko: ".$_POST['suggestion_title']."\n\n";
+    $content .= "Vinkin otsikko: ".$suggestion_title."\n\n";
 
     $content .= "Vinkin sisältö: ".$suggestion_content."\n\n";
 
@@ -232,7 +238,6 @@ if (   $_SERVER['REQUEST_METHOD'] === 'POST'
     $content .= "Kieli: ".$lang_key."\n\n";
 
     $content .= "Lue: " . get_site_url()."/wp-admin/post.php?post=".$suggestion_id."&action=edit";
-
 
     wp_mail( $emails_str, "[POF] Uusi vinkki", $content, 'From: "' . pof_settings_get_suggestions_email_sender_name() . '" <'.pof_settings_get_suggestions_email_sender_email().'>');
 
