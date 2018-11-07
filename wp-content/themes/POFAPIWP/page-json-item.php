f7d3c1fc6f89c0b317f3aad640b7e9d6ae5242c0
Eetu <eetu.nuorva@gmail.com>
2018-11-07 10:26:56 +0200

    Show additional task groups only if item is a task

--- wp-content/themes/POFAPIWP/page-json-item.php
+++ wp-content/themes/POFAPIWP/page-json-item.php
@@ -64,7 +64,9 @@ switch ($post_type) {
 $jsonItem = new $post_class;
 $jsonItem->type = $post_type;
 
-$jsonItem->task_groups = pof_get_additional_taskgroups($mypost->ID);
+if($post_type == 'pof_post_task') {
+	$jsonItem->task_groups = pof_get_additional_taskgroups($mypost->ID);
+}
 
 if (empty($tree_array)) {
 	$jsonItem->parents = array();
